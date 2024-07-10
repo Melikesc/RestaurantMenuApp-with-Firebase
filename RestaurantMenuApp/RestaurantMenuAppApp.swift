361e6cf7d30b363545c034d68e0187741dffbd56
Melike Sultan CAN <melikesultan027@gmail.com>
2024-07-10 08:53:26 +0300

    Initial Commit

---
+++ RestaurantMenuApp/RestaurantMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantMenuAppApp.swift
+//  RestaurantMenuApp
+//
+//  Created by Melike Sultan CAN on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
